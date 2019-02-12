f3c67ad615a79bb6ece07ededb582e6b7f568b96
heytitle <pat.chormai@gmail.com>
2019-02-13 00:13:00 +0100

    update script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-GIT_DEPLOY_REPO=git@gitlab.com:elect.in.th/zone-and-candidate-info.git
-GIT_DEPLOY_BRANCH=deploy
-GIT_DEPLOY_DIR=dist
+export GIT_DEPLOY_REPO=git@gitlab.com:elect.in.th/zone-and-candidate-info.git
+export GIT_DEPLOY_BRANCH=deploy
+export GIT_DEPLOY_DIR=dist 
+
 python generate-site.py --env prod \
 && ./scripts/push-page.sh
